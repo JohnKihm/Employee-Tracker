3232aa7aca2cfbf0c34b88b94d16949278630012
JohnKihm <john.a.kihm@gmail.com>
2024-06-24 13:08:32 -0500

    added view all query to index.js

--- index.js
+++ index.js
@@ -4,53 +4,54 @@ require('dotenv').config();
 
 const pool = new Pool(
     {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      host: '127.0.0.1',
-      database: process.env.DB_NAME
-  });
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: '127.0.0.1',
+        database: process.env.DB_NAME
+    }
+);
   
-  pool.connect();
+// pool.connect();
 
 function init() {
-    let cont = true;
-    while (cont) {
-        inquirer.prompt([
-            {
-                type: 'list',
-                message: 'What would you like to do?',
-                name: 'action',
-                choices: ['View All Employees',
-                    'Add Employee',
-                    'Update Employee Role',
-                    'View All Roles',
-                    'Add Role',
-                    'View All Departments',
-                    'Add Department',
-                    'Quit'
-                ]
-            }
-        ]).then((data) => {
-            switch (data.action) {
-                case 'View All Employees':
-                    break;
-                case 'Add Employee':
-                    break;
-                case 'Update Employee Role':
-                    break;
-                case 'View All Roles':
-                    break;
-                case 'Add Role':
-                    break;
-                case 'View All Departments':
-                    break;
-                case 'Add Department':
-                    break;
-                case 'Quit':
-                    cont = false;
-            }
-        });
-    }
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'action',
+            choices: ['View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit'
+            ]
+        }
+    ]).then((data) => {
+        switch (data.action) {
+            case 'View All Employees':
+            case 'View All Roles':
+            case 'View All Departments':
+                const table = data.action.split(' ')[2].toLowerCase();
+                pool.query(`SELECT * FROM ${table}`, (err, {rows}) => {
+                    console.log(rows);
+                });
+                break;
+            case 'Add Employee':
+                break;
+            case 'Update Employee Role':
+                break;
+            case 'Add Role':
+                break;
+            case 'Add Department':
+                break;
+            case 'Quit':
+                return;
+        }
+        init();
+    });
 }
 
 // Function call to initialize app
