5596ee46db816acb99acad4a0dca2e9c965c0688
JohnKihm <john.a.kihm@gmail.com>
2024-06-24 00:10:54 -0500

    laid out basic structure of init function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -8,8 +8,50 @@ const pool = new Pool(
       password: process.env.DB_PASSWORD,
       host: '127.0.0.1',
       database: process.env.DB_NAME
-  },
-  console.log('Connected to the courses_db database!')
-  )
+  });
   
   pool.connect();
+
+function init() {
+    let cont = true;
+    while (cont) {
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'action',
+                choices: ['View All Employees',
+                    'Add Employee',
+                    'Update Employee Role',
+                    'View All Roles',
+                    'Add Role',
+                    'View All Departments',
+                    'Add Department',
+                    'Quit'
+                ]
+            }
+        ]).then((data) => {
+            switch (data.action) {
+                case 'View All Employees':
+                    break;
+                case 'Add Employee':
+                    break;
+                case 'Update Employee Role':
+                    break;
+                case 'View All Roles':
+                    break;
+                case 'Add Role':
+                    break;
+                case 'View All Departments':
+                    break;
+                case 'Add Department':
+                    break;
+                case 'Quit':
+                    cont = false;
+            }
+        });
+    }
+}
+
+// Function call to initialize app
+init();
