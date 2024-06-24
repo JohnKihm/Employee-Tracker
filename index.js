a26431593ad0ca947f0625ed51ddff0f4d2b055e
JohnKihm <john.a.kihm@gmail.com>
2024-06-23 23:23:16 -0500

    connected pool in index.js and created .env file

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const { Pool } = require('pg');
+require('dotenv').config();
 
+const pool = new Pool(
+    {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: '127.0.0.1',
+      database: process.env.DB_NAME
+  },
+  console.log('Connected to the courses_db database!')
+  )
+  
+  pool.connect();
